1ddc1af282110beb818e1ad432fa03dfcb42fe13
Caio Lincoln <clincolnrs@gmail.com>
2024-06-14 22:21:19 -0300

    Varias alterações na area de treinamentos e com o nltk

---
+++ backend/app/files/treinamento.py
@@ -0,0 +1,54 @@
+import json
+import os
+from sentence_transformers import SentenceTransformer, InputExample, losses
+from torch.utils.data import DataLoader
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+# Carregar o modelo pré-treinado
+# model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+model = SentenceTransformer(os.getenv("FOLDER")+"backend/modelos/2024_06_14_v2")
+
+# Carregar dados de treinamento a partir de um arquivo JSON
+with open(os.getenv("FOLDER")+'backend/app/files/treinamento_perguntas.json', 'r', encoding='utf-8') as f:
+    treinamento_perguntas = json.load(f)
+
+# Preparar exemplos para treinamento
+train_examples = []
+
+for item in treinamento_perguntas:
+    perguntas = item['perguntas']
+    label = float(item['label'])
+    
+    # Comparar a pergunta1 com as outras perguntas
+    pergunta1 = perguntas[0]
+    for i in range(1, len(perguntas)):
+        train_examples.append(InputExample(texts=[pergunta1, perguntas[i]], label=label))
+
+# Criar um DataLoader para o treinamento
+train_dataloader = DataLoader(train_examples, shuffle=False, batch_size=2)
+
+# Definir a loss function
+train_loss = losses.CosineSimilarityLoss(model)
+
+# Treinar o modelo
+# 50 epochs = 5 warmup_steps
+# 100 perguntas * 50 epochs = 5000 perguntas 
+# 100 perguntas * 5 warmup_steps = 500 perguntas
+# total de perguntas que serão contabilizadas 4500
+# definir o warmup entre 5% a 10% do total de treinamento
+model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=50, warmup_steps=5)
+
+# Diretório para salvar o modelo treinado
+
+output_dir = os.getenv("FOLDER")+"backend/modelos/2024_06_14_v3"
+
+# Verificar se o diretório existe, caso contrário, criá-lo
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Salvar o modelo treinado
+model.save(output_dir)
+
+print(f"Modelo treinado e salvo no diretório '{output_dir}'.")
--- backend/app/services/chat_service.py
+++ backend/app/services/chat_service.py
@@ -1,32 +1,49 @@
 from ..models.chat import Chat
 import json
 import numpy as np
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
+# from nltk.corpus import stopwords
+# from nltk.tokenize import word_tokenize
+# from nltk.stem import WordNetLemmatizer
 from sentence_transformers import SentenceTransformer, util
+from dotenv import load_dotenv
+import os
 
 class ChatService:
     
     def __init__(self):
-        chat = Chat('paraphrase-MiniLM-L6-v2')
+        load_dotenv()
+        chat = Chat(os.getenv("FOLDER") + 'backend/modelos/2024_06_14_v2')
         self.model = SentenceTransformer(chat.engine_name)
-        self.stop_words = set(stopwords.words('portuguese'))
-        self.lemmatizer = WordNetLemmatizer()
+        # self.stop_words = set(stopwords.words('portuguese'))
+        # self.lemmatizer = WordNetLemmatizer()
 
-    def preprocess_text(self, text):
-        tokens = word_tokenize(text.lower(), language='portuguese')
-        tokens = [self.lemmatizer.lemmatize(token) for token in tokens if token.isalpha() and token not in self.stop_words]
-        return ' '.join(tokens)
+    # def preprocess_text(self, text):
+    #     tokens = word_tokenize(text.lower(), language='portuguese')
+    #     tokens = [self.lemmatizer.lemmatize(token) for token in tokens if token.isalpha() and token not in self.stop_words]
+    #     return ' '.join(tokens)
 
     def get_resposta(self, pergunta_usuario):
-        with open("C:/Users/ymattos/OneDrive/Área de Trabalho/yves/meus docs/TCC ADS/projeto-tcc/tcc_chat/backend/app/files/data.json", 'r', encoding='utf-8') as f:
+        with open(os.getenv("FOLDER") + "backend/app/files/FAQ.json", 'r', encoding='utf-8') as f:
             faq = json.load(f)
-        qa_pairs = [(self.preprocess_text(question), self.preprocess_text(answer)) for question, answer in faq.items()]
+        
+        qa_pairs = [(question, answer) for question, answer in faq.items()]
         question_embeddings = self.model.encode([q for q, a in qa_pairs])
         input_embedding = self.model.encode(pergunta_usuario)
-        similarities = util.pytorch_cos_sim(input_embedding, question_embeddings)[0]
-        best_match_idx = np.argmax(similarities)
-        max_similarity = similarities[best_match_idx].item()
-        return {"pergunta_base": qa_pairs[best_match_idx][0],"resposta": qa_pairs[best_match_idx][1],"similaridade_maxima": max_similarity}
+        similarities = util.pytorch_cos_sim(input_embedding, question_embeddings)[0].numpy()
+
+        # Ordena as similaridades e pega os índices das 3 maiores
+        sorted_indices = np.argsort(similarities)[::-1]
+        top_3_indices = sorted_indices[:3]
+
+        top_3_res = []
+
+        for idx in top_3_indices:
+            similarity = similarities[idx].item()
+            result = {
+                "pergunta_base": qa_pairs[idx][0],
+                "resposta": qa_pairs[idx][1],
+                "similaridade_maxima": similarity
+            }
+            top_3_res.append(result)
+
+        return top_3_res
