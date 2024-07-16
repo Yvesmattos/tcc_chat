a898dceea84065af910cd6ed36618180cfd4ea44
yvesmattos@eadskill.com.br <yvesmattos@eadskill.com.br>
2024-07-15 21:20:07 -0300

    fix: ajuste para build

--- chat-frontend/src/index.js
+++ chat-frontend/src/index.js
@@ -1,9 +1,15 @@
+// src/index.js
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
+// Função de inicialização do React App
 function initialize(containerId) {
   const container = document.getElementById(containerId);
+  if (!container) {
+    console.error(`Container with ID ${containerId} not found.`);
+    return;
+  }
   const root = createRoot(container);
   root.render(<App />);
 }
